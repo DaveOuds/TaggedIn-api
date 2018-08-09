504c1b3340c1078f02b931bd36a426017eacb2a0
Davey Oudshoorn (admin) <doudshoorn-a@doudshoorn-l2.ffx.bixal.net>
2018-08-09 11:09:57 -0400

    Refactored url for proxy server handling

--- TaggedInApi/urls.py
+++ TaggedInApi/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include('contact.urls')),
-    url(r'^', include('user.urls')),
+    url(r'^api/', include('contact.urls')),
+    url(r'^api/', include('user.urls')),
     url(r'^admin/', admin.site.urls),
 ]
