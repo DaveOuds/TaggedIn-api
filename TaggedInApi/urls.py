3d1bb2d3c4b6337eee223b9f2123d4bd2ea2df59
Davey Oudshoorn (admin) <doudshoorn-a@doudshoorn-l2.ffx.bixal.net>
2018-08-09 13:25:45 -0400

    Revert "Refactored url for proxy server handling"
    This reverts commit 504c1b3340c1078f02b931bd36a426017eacb2a0.

--- TaggedInApi/urls.py
+++ TaggedInApi/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^api/', include('contact.urls')),
-    url(r'^api/', include('user.urls')),
+    url(r'^', include('contact.urls')),
+    url(r'^', include('user.urls')),
     url(r'^admin/', admin.site.urls),
 ]
